424caff4fa2e8ff3e0f16388520ab356a72141dd
Your Name <you@example.com>
2022-01-18 14:22:58 -0700

    Fourth Commit

--- CarfulWeb/main/default/lwc/vehicleCatalogue/vehicleCatalogue.js
+++ CarfulWeb/main/default/lwc/vehicleCatalogue/vehicleCatalogue.js
@@ -1,5 +1,5 @@
 import { LightningElement, api, track, wire } from 'lwc';
-import getVehicles from '@salesforce/apex/dataCatalogue.getVehicles';
+import getVehicles from '@salesforce/apex/catalogueData.getVehicles';
 
 
 export default class Catalogue extends LightningElement {
